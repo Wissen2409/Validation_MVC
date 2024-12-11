2a2f3d61d3b65fe878ee241fddd6ce66b1a071e6
emrahelis <emrahelis@gmail.com>
2024-12-11 20:40:25 +0300

    mkn

--- Models/UserViewModel.cs
+++ Models/UserViewModel.cs
@@ -13,4 +13,15 @@ public class UserViewModel
     [Required(ErrorMessage = "Email alanı boş olamaz")]
     [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$",ErrorMessage ="Email adres uygun değil")]
     public string Email { get; set; }
+
+    [Range(10,70,ErrorMessage ="Lütfen 10 ile 70 arasında bir değer giriniz")]
+    [Required(ErrorMessage ="Lütfen yaş değerini giriniz")]
+    public int Age{ get; set; } 
+
+    // iki propery'nin değerinin birbirinin aynısı olması için yapılan validasyon 
+    [Required]
+    public string Password { get; set; }
+
+    [Compare("Password", ErrorMessage = "Girilen değerler aynı değil")]
+    public string ConfirmPassword { get; set; }
 }
